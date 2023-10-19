43308a2b67fb4ba2d4181d6fe77828d099fa08b5
Dlima15 <danilo.lima7456@gmail.com>
2023-10-19 19:34:19 -0300

    Add item na lista

---
+++ 0009.py
@@ -0,0 +1,13 @@
+print("Função C/ argumento fixo")
+
+def cnt(Z=2):
+    print(Z*5)
+
+cnt()
+
+cnt(5)
+
+
+
+
+
---
+++ 010.py
@@ -0,0 +1,11 @@
+A = ['Nome','Carlos']
+
+def entryname(x):
+    A.append(x)
+
+print("Entre com nome")
+m = input("Qual é seu nome?: ")
+
+entryname(m)
+
+print(A)
