ac92d87dc29551e27c5c615560ccbc8a0b6df9cc
Dlima15 <danilo.lima7456@gmail.com>
2023-10-25 20:56:26 -0300

    Impar ou Par

---
+++ 016.py
@@ -0,0 +1,12 @@
+def imparOuPar(x): 
+
+    if x%2 == 0:
+        print("Número é par")
+
+
+    else :
+        print('númer é Impar')
+
+Number = int(input("Qual o numero?: "))
+
+imparOuPar(Number)
